f7242b6defb1c1e4b96bc5bce9fdddff31dcadb3
Heejin Na <heejin.info@gmail.com>
2021-12-02 16:14:42 +0900

    :construction: Implement deleteTask function

--- src/js/index.js
+++ src/js/index.js
@@ -18,9 +18,16 @@ function addContent(value) {
   li.innerHTML = `${value}<button id='delete' class='btn' type='button'>X</button>`;
 }
 
+function deleteTask(e) {
+  const li = e.target.parentNode;
+  const ul = li.parentNode;
+  ul.removeChild(li);
+}
+
 $addBtn.addEventListener('click', addTask);
 $input.addEventListener('keyup', (e) => {
   if (e.key === 'Enter') {
     addTask(e);
   }
 });
+$list.addEventListener('click', deleteTask);
