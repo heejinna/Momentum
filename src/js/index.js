9ef23aa8d60f859c584976206e71ca0da6a6a5ab
Heejin Na <heejin.info@gmail.com>
2021-12-02 15:56:17 +0900

    :construction: Implement addTask function

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const $input = document.querySelector('input');
+const $addBtn = document.querySelector('.add');
+const $list = document.querySelector('#list');
+
+function addTask(e) {
+  e.preventDefault();
+  let value = $input.value;
+  value !== '' ? addContent(value) : alert('please type your task first');
+  $input.value = '';
+}
+
+function addContent(value) {
+  const $ul = document.querySelector('ul');
+  const li = document.createElement('li');
+  const btn = document.createElement('button');
+  li.setAttribute('class', 'todo');
+  $ul.appendChild(li);
+  li.innerHTML = `${value}<button id='delete' class='btn' type='button'>X</button>`;
+}
+
+$addBtn.addEventListener('click', addTask);
+$input.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    addTask(e);
+  }
+});
